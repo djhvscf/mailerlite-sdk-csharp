de40ae619be3d67e5e2e1c93d9bf59fc4f5feb47
Dennis Hernández <dennishernandezvargas@gmail.com>
2017-05-26 23:47:05 -0600

    Adding more endpoints for groups section

--- src/mailerlite-sdk-csharp/Api/Groups.cs
+++ src/mailerlite-sdk-csharp/Api/Groups.cs
@@ -36,7 +36,7 @@ namespace mailerlite_sdk_csharp.Api
 
         public Stream Create(string name)
         {
-            if(name.IsNullOrEmpty())
+            if (name.IsNullOrEmpty())
             {
                 throw new MailerLiteException("Group name cannot be empty");
             }
@@ -54,7 +54,8 @@ namespace mailerlite_sdk_csharp.Api
                 throw new MailerLiteException("Group name cannot be empty");
             }
 
-            return this.Http.Put($"{Url}/{groupId}", new {
+            return this.Http.Put($"{Url}/{groupId}", new
+            {
                 name = name
             }, HttpContentTypes.ApplicationJson).ResponseStream;
         }
@@ -72,6 +73,49 @@ namespace mailerlite_sdk_csharp.Api
         public Stream GetSubscribersCountByGroupId(int groupId)
         {
             return this.Http.Get($"{Url}/{groupId}/subscribers/count").ResponseStream;
-        }   
+        }
+
+        public Stream GetSubscribersByTypeByGroupId(int groupId, CampaignSubscriberType type, string filters,
+            int offset, int limit)
+        {
+            return this.Http.Get(this.AddQueryParams($"{Url}/{groupId}/subscribers/{type.ToString()}", limit: limit,
+                offset: offset, filters: filters)).ResponseStream;
+        }
+
+        public Stream GetSubscribersCountByTypeByGroupId(int groupId, CampaignSubscriberType type)
+        {
+            return this.Http.Get($"{Url}/{groupId}/subscribers/{type.ToString()}/count").ResponseStream;
+        }
+
+        public Stream GetSubscribersByGroupId(int groupId, int subscriberId)
+        {
+            return this.Http.Get($"{Url}/{groupId}/subscribers/{subscriberId}").ResponseStream;
+        }
+
+        public Stream DeleteSubscriberByGroupId(int groupId, int? subscriberId, string email)
+        {
+            if (subscriberId.HasValue)
+            {
+                return this.Http.Delete($"{Url}/{groupId}/subscribers/{subscriberId}").ResponseStream;
+            }
+            else
+            {
+                return this.Http.Delete($"{Url}/{groupId}/subscribers/{email}").ResponseStream;
+            }
+        }
+
+        public Stream AddSubscriberToGroup(int groupId, string email, string name, List<string> fields,
+            bool resubcribe, bool autoresponders, AddCampaignSubscriberType type)
+        {
+            return this.Http.Post($"{Url}/{groupId}/subscribers", new
+            {
+                email = email,
+                name = name,
+                fields = fields,
+                resubcribe = resubcribe,
+                autoresponders = autoresponders,
+                type = type.ToString()
+            }, HttpContentTypes.ApplicationJson).ResponseStream;
+        }
     }
 }
